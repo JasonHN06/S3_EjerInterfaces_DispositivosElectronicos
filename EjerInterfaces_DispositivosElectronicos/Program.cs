b4b44adc93c93c4db1d30d91a65783fd2de650e0
Jason Madrid <jasonhnn6@gmail.com>
2025-08-09 18:07:55 -0600

    adding data

--- EjerInterfaces_DispositivosElectronicos/Program.cs
+++ EjerInterfaces_DispositivosElectronicos/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using EjerInterfaces_DispositivosElectronicos;
+
+Telefono telefono = new Telefono();
+telefono.Marca = "Samsung";
+telefono.Modelo = "Galaxy S25";
+telefono.Apagar();
+
+Laptop laptop = new Laptop();
+laptop.Marca = "Lenovo";
+laptop.Modelo = "IdeaPad 3";
+laptop.Apagar();
+laptop.NivelBateria = 95;
+
+Console.WriteLine();
+Console.WriteLine("*** Estado inicial de los dispositivos ***");
+telefono.MostrarEstado();
+laptop.MostrarEstado();
+Console.WriteLine();
+
+Console.WriteLine("Encendiendo los dispositivos...");
+telefono.Encender();
+laptop.Encender();
+Console.WriteLine();
+
+Console.WriteLine("*** Estado actual de los dispositivos ***");
+telefono.MostrarEstado();
+laptop.MostrarEstado();
+Console.WriteLine();
+
+Console.WriteLine("Apagando los dispositivos...");
+telefono.Apagar();
+laptop.Apagar();
+Console.WriteLine();
+
+Console.WriteLine("*** Estado final de los dispositivos ***");
+telefono.MostrarEstado();
+laptop.MostrarEstado();
+Console.WriteLine();
