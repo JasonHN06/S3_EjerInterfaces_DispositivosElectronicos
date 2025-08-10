f0e3e1801a81f47e6d5e9efaa42d57264dbbaba1
Jason Madrid <jasonhnn6@gmail.com>
2025-08-09 18:08:05 -0600

    create class

---
+++ EjerInterfaces_DispositivosElectronicos/Laptop.cs
@@ -0,0 +1,27 @@
+﻿namespace EjerInterfaces_DispositivosElectronicos
+{
+    public class Laptop
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public bool EstaEncendida { get; set; }
+        public int NivelBateria { get; set; }
+        public void Encender()
+        {
+            EstaEncendida = true;
+            Console.WriteLine($"La laptop {Marca} {Modelo} está encendida.");
+        }
+
+        public void Apagar()
+        {
+            EstaEncendida = false;
+            Console.WriteLine($"La laptop {Marca} {Modelo} está apagada.");
+        }
+
+        public void MostrarEstado()
+        {
+            string estado = EstaEncendida ? "encendida" : "apagada";
+            Console.WriteLine($"La laptop {Marca} {Modelo} está {estado} con un nivel de batería del {NivelBateria}%.");
+        }
+    }
+}
---
+++ EjerInterfaces_DispositivosElectronicos/Telefono.cs
@@ -0,0 +1,27 @@
+﻿namespace EjerInterfaces_DispositivosElectronicos
+{
+    public class Telefono
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public bool EstaEncendido { get; set; }
+
+        public void Encender()
+        {
+            EstaEncendido = true;
+            Console.WriteLine($"El teléfono {Marca} {Modelo} está encendido.");
+        }
+
+        public void Apagar()
+        {
+            EstaEncendido = false;
+            Console.WriteLine($"El teléfono {Marca} {Modelo} está apagado.");
+        }
+
+        public void MostrarEstado()
+        {
+            string estado = EstaEncendido ? "encendido" : "apagado";
+            Console.WriteLine($"El teléfono {Marca} {Modelo} está {estado}.");
+        }   
+    }
+}
